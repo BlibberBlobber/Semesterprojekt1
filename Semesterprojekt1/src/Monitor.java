0c53dcfc2dc53e7aaf4da5bb7998a9de41421799
BlibberBlobber <Mortenvorborg@hotmail.com>
2017-01-10 14:43:32 +0100

    jo

--- Semesterprojekt1/src/Monitor.java
+++ Semesterprojekt1/src/Monitor.java
@@ -6,19 +6,13 @@ public class Monitor {
 	private static double puls;
 	public static void main(String[] args) throws InterruptedException {
 		
-		//førstetest
-		// test for git
-		//heodjednk
-		//instantiere reciever objektet s� vi �bner porten og g�r klar til at l�se
 		Reciever reciever = new Reciever();
-		// instantiere FileWriter objektet s� vi kan skrive til en txt fil p� computeren
 		FileWriter filewriter = new FileWriter();
-		// instantiere converter objektet, s� vi kan f� omregnet alle vores v�rdier til en puls
 		Converter converter = new Converter();
 			
-		//l�kken vi er i s� l�nge vi m�ler {
+		//løkken vi er i så længe vi måler {
 			
-			// laver en liste med 600 double v�rdier i
+			// laver en liste med 600 double værdier i
 			list = reciever.getValue();
 			
 			// giver converter listen med de 600 tal
