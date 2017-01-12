0518df972815b84294c00cdb1f2d906f24311c74
BlibberBlobber <Mortenvorborg@hotmail.com>
2017-01-12 13:51:48 +0100

    FileWriter, der skriver de nutz ud man kunne tænke sig

--- Semesterprojekt1/src/OurFileWriter.java
+++ Semesterprojekt1/src/OurFileWriter.java
@@ -1,75 +1,26 @@
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class OurFileWriter {
 	
-	private String fileName = "Puls.txt";
-	private String path = "";
-	private double puls;
-	public boolean append = false;
+	//private String path = "C:\\Users\\Morten\\PulsMålinger\\Nåtz.txt";
 	
-	public void OurFileWriter() throws IOException {
-		if(!doFileExist(fileName)) makeEmptyFile();
+	// constructor
+	public OurFileWriter() {
 	}
 	
-	public void writeDoubleToFile(double puls){
+	public void writeDoubleToFile(double puls, String path) throws IOException{
 		
-		// s?t ind hvis filen er tom ellers s? g? en linje ned og skriv
-		// 
+		// Instantier alle filskriverne på én linje
+		BufferedWriter bf = new BufferedWriter(new FileWriter(new File(path),true));
+		
+		//skriv hvad vi har lyst til
+		bf.write("Din puls er " + String.valueOf(puls) + "\n");
+		
+		// close dat fuckboi
+		bf.close();
 	}
 	
-	public void makeEmptyFile() throws IOException{
-		FileWriter fw = new FileWriter(fileName, append = true);
-
-	    BufferedWriter bw = new BufferedWriter(fw);
-	    PrintWriter pw = new PrintWriter(bw);
-	    pw.println(bw);
-	    pw.close();
-	    // System.out.println("Skrevet til fil:"+input);
-	}
-		
-		
-		
-		//brug fileName og path
-		// laver en tom txt fil i path
-	
-	public boolean doFileExist(String fileName){
-		
-		// check om filen med navnet fileName findes p? path 
-		
-		return true;
-	}
 }
-
-/*
-public class OurFileWriter {
-
-private String fileName = "Puls.txt";
-private String path = "";
-private double puls;
-
-public OurFileWriter() {
-	if(!doFileExist(fileName)) makeEmptyFile();
-}
-
-public void writeDoubleToFile(double puls){
-	
-	// s�t ind hvis filen er tom ellers s� g� en linje ned og skriv
-	
-}
-
-public void makeEmptyFile(){
-	// brug fileName og path
-	// laver en tom txt fil i path
-}
-
-public boolean doFileExist(String fileName){
-	
-	// check om filen med navnet fileName findes p� path 
-	
-	return true;
-}
-}
-*/
